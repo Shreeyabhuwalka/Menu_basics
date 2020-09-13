c2b76047716f92711492ac0fc8f941bc7ee42102
Shreeya <shreeyabhuwalka@gmail.com>
2020-09-13 21:32:25 +0530

    Context menu added

--- app/src/main/java/com/developer/menu/MainActivity.java
+++ app/src/main/java/com/developer/menu/MainActivity.java
@@ -4,17 +4,25 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //context menu
+        btn = (Button) findViewById(R.id.button);
+        registerForContextMenu(btn);
     }
 
     @Override
@@ -47,5 +55,39 @@ public class MainActivity extends AppCompatActivity {
         }
         Toast.makeText(this, "Nothing selected!", Toast.LENGTH_SHORT).show();
         return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu,menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.item1:
+                Toast.makeText(this, "Item 1 selected!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item2:
+                Toast.makeText(this, "Item 2 selected!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item3:
+                Toast.makeText(this, "Item 3 selected!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.subitem1:
+                Toast.makeText(this, "Sub-Item 1 selected!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.subitem2:
+                Toast.makeText(this, "Sub-Item 2 selected!", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+//        Toast.makeText(this, "Nothing selected!", Toast.LENGTH_SHORT).show();
+
+        return super.onContextItemSelected(item);
     }
 }
